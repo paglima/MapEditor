d2529dfdc0753ba0bd3595ee0219f987aa802ecd
paglima <paulopagl@gmail.com>
2016-01-16 16:51:12 -0200

    Carregando json com informacoes de sprites

---
+++ src/json/JsonLabel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package json;
+
+/**
+ *
+ * @author pauloarthur
+ */
+public class JsonLabel {
+    
+    private String name;
+    private String path;
+    private int positionX;
+    private int positionY;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
